2cfa8f8d44528f0ee1e271f52540fae1eea03d58
MasaharuDobashi <dobashi.masaharu@gmail.com>
2023-10-22 14:40:14 +0900

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by 土橋正晴 on 2023/10/22.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
